91f49fadca2fb0acdfe5342fc7743ac3e6d0e745
Alex Ott <alexott@gmail.com>
2022-09-17 20:14:32 +0200

    fix test

--- tests/test_pipelines_databricks_sigma.py
+++ tests/test_pipelines_databricks_sigma.py
@@ -1,5 +1,5 @@
 import pytest
 from sigma.backends.databricks import DatabricksSigmaBackend
-from sigma.pipelines.databricks import # TODO: import pipeline functions
+#from sigma.pipelines.databricks import # TODO: import pipeline functions
 
 # TODO: import tests for all implemented pipelines and contained transformations
