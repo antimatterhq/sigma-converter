fd2452d4e69ca264488e0de6ac98586a35f71200
Alex Ott <alexott@gmail.com>
2024-07-14 11:18:32 +0200

    Add tests for pipelines, bump pysigma dependency
    Changes include:
    
    - bump PySigma to 0.11.8 to use `FieldFunctionTransformation`, and remove this
      implementation from my code
    - added tests for `snake_case` transformer

--- sigma/pipelines/databricks/snake_case.py
+++ sigma/pipelines/databricks/snake_case.py
@@ -3,35 +3,7 @@ from typing import Union, List, Dict, Callable
 
 import inflection
 from sigma.processing.pipeline import ProcessingItem, ProcessingPipeline
-from sigma.processing.transformations import FieldMappingTransformationBase
-from sigma.rule import SigmaDetectionItem
-
-
-# TODO: remove it when the PR is merged into PySigma
-@dataclass
-class FieldFunctionTransformation(FieldMappingTransformationBase):
-    """Map a field name to another using provided transformation function.
-    You can overwrite transformation by providing explicit mapping for a field."""
-
-    transform_func: Callable[[str], str]
-    mapping: Dict[str, str] = field(default_factory=lambda: {})
-
-    def _transform_name(self, f: str) -> str:
-        if f:
-            return self.mapping.get(f, self.transform_func(f))
-        return f
-
-    def apply_detection_item(self, detection_item: SigmaDetectionItem):
-        super().apply_detection_item(detection_item)
-        f = detection_item.field
-        mapping = self._transform_name(f)
-        if self.processing_item.match_field_name(self.pipeline, f):
-            self.pipeline.field_mappings.add_mapping(f, mapping)
-            detection_item.field = mapping
-            self.processing_item_applied(detection_item)
-
-    def apply_field_name(self, f: str) -> Union[str, List[str]]:
-        return [self._transform_name(f)]
+from sigma.processing.transformations import FieldFunctionTransformation
 
 
 # Processing pipelines should be defined as functions that return a ProcessingPipeline object.
--- tests/test_pipelines_databricks_sigma.py
+++ tests/test_pipelines_databricks_sigma.py
@@ -1,5 +1,32 @@
-import pytest
 from sigma.backends.databricks import DatabricksBackend
-#from sigma.pipelines.databricks import # TODO: import pipeline functions
+from sigma.pipelines.databricks import snake_case
+from sigma.collection import SigmaCollection
+from sigma.rule import SigmaRule
 
-# TODO: import tests for all implemented pipelines and contained transformations
+YAML_STR = """
+            title: Test
+            status: test
+            logsource:
+                product: windows
+                service: security
+            detection:
+                sel:
+                    EventID: 123
+                    Image: test.exe
+                    TestField: test
+                condition: sel
+            fields:
+                - EventID
+                - TestField
+        """
+
+
+def test_snake_case_fields_conversion():
+    rule = snake_case().apply(SigmaRule.from_yaml(YAML_STR))
+    assert rule.fields == ["event_id", "test_field"]
+
+
+def test_snake_case_detection_conversion():
+    rule = DatabricksBackend(snake_case()).convert(
+        SigmaCollection.from_yaml(YAML_STR))
+    assert rule == ["event_id = 123 AND lower(image) = lower('test.exe') AND lower(test_field) = lower('test')"]
